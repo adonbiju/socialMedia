116ff1d44f19619ee4dfad5565989e8417ca40ff
adonbiju <adonbiju@gmail.com>
2023-01-17 10:31:14 +0530

    In backend, "/post/:userId/posts" route has been completed

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,15 @@
 
 import express from "express";
-import { getFeedPosts} from "../controllers/posts.js";
+import { 
+    getFeedPosts,
+    getUserPosts
+    }from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 /* READ */
 router.get("/", verifyToken, getFeedPosts);
-
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
 export default router;
