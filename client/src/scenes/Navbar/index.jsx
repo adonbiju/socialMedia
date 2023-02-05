9db821485c6980b3eb6c84121f0fe2570f225f70
adonbiju <adonbiju@gmail.com>
2023-02-05 19:35:14 +0530

    In Navbar ==>search Widget is added for Non mobile screen

--- client/src/scenes/Navbar/index.jsx
+++ client/src/scenes/Navbar/index.jsx
@@ -58,7 +58,21 @@ const Navbar = () => {
         >
           Sociopedia
         </Typography>
+        {isNonMobileScreens && (
+          <FlexBetween
+            backgroundColor={neutralLight}
+            borderRadius="9px"
+            gap="3rem"
+            padding="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        )}
     </FlexBetween>
+    
     </FlexBetween>
   )
 }
