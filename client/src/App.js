7a405bef1a8963a74f6b926d87b0e55cbf22911c
adonbiju <adonbiju@gmail.com>
2023-02-05 18:41:02 +0530

    theme settings has been added in the App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,26 @@ import LoginPage from "scenes/LoginPage";
 import HomePage from "scenes/HomePage";
 import ProfilePage from "scenes/ProfilePage";
 
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
      <BrowserRouter>
+     <ThemeProvider theme={theme}>
+     <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="/home" element={ <HomePage />}/>
             <Route path="/profile/:userId" element={<ProfilePage />} />
           </Routes>
+      </ThemeProvider>
       </BrowserRouter>
     </div>
   );
