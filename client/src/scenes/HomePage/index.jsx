68d39760dae7d036774ab6729acfd8e5a9add809
adonbiju <adonbiju@gmail.com>
2023-02-06 22:52:13 +0530

    MyPostWidget is Added in the Home Page

--- client/src/scenes/HomePage/index.jsx
+++ client/src/scenes/HomePage/index.jsx
@@ -1,24 +1,34 @@
 import { Box, useMediaQuery } from "@mui/material";
 import Navbar from 'scenes/Navbar'
 import UserWidget from 'scenes/widgets/UserWidget'
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+
 import { useSelector } from "react-redux";
 const HomePage = () => {
   const { _id, picturePath } = useSelector((state) => state.user);
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   return (
-    <div>
+  <Box>
     <Navbar/>
     <Box
-     width="100%"
-     padding="2rem 6%"
-     display={isNonMobileScreens ? "flex" : "block"}
-     gap="0.5rem"
-     justifyContent="space-between">
+      width="100%"
+      padding="2rem 6%"
+      display={isNonMobileScreens ? "flex" : "block"}
+      gap="0.5rem"
+      justifyContent="space-between">
         <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
           <UserWidget userId={_id} picturePath={picturePath} />
         </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+      
+        </Box>
+        
     </Box>
-    </div>
+  </Box>
   )
 }
 
