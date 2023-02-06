e5d684446fc123bddaa13169ae62647609249ad7
adonbiju <adonbiju@gmail.com>
2023-02-06 07:50:20 +0530

    In Login Page, Another Box is created to show the Form of the Login Page

--- client/src/scenes/LoginPage/index.jsx
+++ client/src/scenes/LoginPage/index.jsx
@@ -1,4 +1,5 @@
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
 const LoginPage = () => {
 
@@ -18,7 +19,18 @@ const LoginPage = () => {
         </Typography>
       </Box>
 
-     
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Socipedia, the Social Media for Sociopaths!
+        </Typography>
+       <Form/>
+      </Box>
     </Box>
   )
 }
