0c7586f0a0d6315994becb51ef29cf3174201055
adonbiju <adonbiju@gmail.com>
2023-02-06 12:15:15 +0530

    In App.js , checked the Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "scenes/LoginPage";
 import HomePage from "scenes/HomePage";
 import ProfilePage from "scenes/ProfilePage";
@@ -12,6 +12,7 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="App">
      <BrowserRouter>
@@ -19,8 +20,8 @@ function App() {
      <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={ <HomePage />}/>
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route path="/home" element={isAuth? <HomePage />:  <Navigate to="/" /> }/>
+            <Route path="/profile/:userId" element={isAuth? <ProfilePage />:  <Navigate to="/" />} />
           </Routes>
       </ThemeProvider>
       </BrowserRouter>
