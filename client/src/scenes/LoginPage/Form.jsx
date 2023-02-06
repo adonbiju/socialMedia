bda1f595ed8e7c8cf93d88efff0c600396c13efb
adonbiju <adonbiju@gmail.com>
2023-02-06 11:16:01 +0530

    In Form.js ,Form submit of "registet function " Completed.

--- client/src/scenes/LoginPage/Form.jsx
+++ client/src/scenes/LoginPage/Form.jsx
@@ -56,8 +56,39 @@ const Form = () => {
   const isLogin = pageType === "login";
   const isRegister = pageType === "register";
 
+  const register = async (values, onSubmitProps) =>{
+    // this allows us to send form info with image
+    const formData = new FormData();
+    for (let value in values) {
+      formData.append(value, values[value]);
+    }
+    formData.append("picturePath", values.picture.name);
+
+    const savedUserResponse = await fetch(
+      "http://localhost:3001/auth/register",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    const savedUser = await savedUserResponse.json();
+    onSubmitProps.resetForm();
+
+    if (savedUser) {
+      setPageType("login");
+    }
+  }
+
+  const login = async (values, onSubmitProps) =>{
+
+  }
+  const handleFormSubmit = async (values, onSubmitProps) => {
+    if (isLogin) await login(values, onSubmitProps);
+    if (isRegister) await register(values, onSubmitProps);
+  };
   return (
     <Formik 
+    onSubmit={handleFormSubmit}
     initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
     validationSchema={isLogin ? loginSchema : registerSchema}>
      {({
