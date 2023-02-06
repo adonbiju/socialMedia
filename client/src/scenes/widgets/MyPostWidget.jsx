ee6b51b2f8299fa04f868b4cecbdc26d41f5766f
adonbiju <adonbiju@gmail.com>
2023-02-06 23:51:22 +0530

    MyPostWidget, Dropzone is added

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -28,7 +28,7 @@ import { setPosts } from "state";
 const MyPostWidget = ({ picturePath }) => {
 
     const dispatch = useDispatch();
-    const [isImage, setIsImage] = useState(false);
+    const [isImage, setIsImage] = useState(true);
     const [image, setImage] = useState(null);
     const [post, setPost] = useState("");
     const { palette } = useTheme();
@@ -79,7 +79,52 @@ const MyPostWidget = ({ picturePath }) => {
                         padding: "1rem 2rem",
                     }}
                 />
-        </FlexBetween>
+            </FlexBetween>
+
+        {isImage && (
+        <Box
+          border={`1px solid ${medium}`}
+          borderRadius="5px"
+          mt="1rem"
+          p="1rem"
+        >
+          <Dropzone
+            acceptedFiles=".jpg,.jpeg,.png"
+            multiple={false}
+            onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
+          >
+            {({ getRootProps, getInputProps }) => (
+              <FlexBetween>
+                <Box
+                  {...getRootProps()}
+                  border={`2px dashed ${palette.primary.main}`}
+                  p="1rem"
+                  width="100%"
+                  sx={{ "&:hover": { cursor: "pointer" } }}
+                >
+                  <input {...getInputProps()} />
+                  {!image ? (
+                    <p>Add Image Here</p>
+                  ) : (
+                    <FlexBetween>
+                      <Typography>{image.name}</Typography>
+                      <EditOutlined />
+                    </FlexBetween>
+                  )}
+                </Box>
+                {image && (
+                  <IconButton
+                    onClick={() => setImage(null)}
+                    sx={{ width: "15%" }}
+                  >
+                    <DeleteOutlined />
+                  </IconButton>
+                )}
+              </FlexBetween>
+            )}
+          </Dropzone>
+        </Box>
+      )}
         </WidgetWrapper>
     )
 }
