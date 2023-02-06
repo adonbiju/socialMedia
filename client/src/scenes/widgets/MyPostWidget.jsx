44bc794ed1d01372ef243cd28631524e3bf7c3b3
adonbiju <adonbiju@gmail.com>
2023-02-06 23:38:23 +0530

    In MyPostWidget, InputBase Design is  added

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -65,8 +65,20 @@ const MyPostWidget = ({ picturePath }) => {
     }
     return(
         <WidgetWrapper>
-        <FlexBetween gap="1.5rem">
-           <UserImage image={picturePath} />
+
+            <FlexBetween gap="1.5rem">
+                <UserImage image={picturePath} />
+                <InputBase
+                    placeholder="What's on your mind..."
+                    onChange={(e) => setPost(e.target.value)}
+                    value={post}
+                    sx={{
+                        width: "100%",
+                        backgroundColor: palette.neutral.light,
+                        borderRadius: "2rem",
+                        padding: "1rem 2rem",
+                    }}
+                />
         </FlexBetween>
         </WidgetWrapper>
     )
