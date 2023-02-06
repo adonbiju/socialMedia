706de707d946418f9a129b4bf21418785b0e1031
adonbiju <adonbiju@gmail.com>
2023-02-06 19:48:53 +0530

    In HomePage, "UserWidget" is added.

--- client/src/scenes/HomePage/index.jsx
+++ client/src/scenes/HomePage/index.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import { Box, useMediaQuery } from "@mui/material";
 import Navbar from 'scenes/Navbar'
 import UserWidget from 'scenes/widgets/UserWidget'
 import { useSelector } from "react-redux";
 const HomePage = () => {
   const { _id, picturePath } = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   return (
     <div>
     <Navbar/>
-    <UserWidget userId={_id} picturePath={picturePath}/>
+    <Box
+     width="100%"
+     padding="2rem 6%"
+     display={isNonMobileScreens ? "flex" : "block"}
+     gap="0.5rem"
+     justifyContent="space-between">
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+    </Box>
     </div>
   )
 }
