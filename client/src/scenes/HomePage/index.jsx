ea4b13c98276df80e17b78f34ab003adf0380426
adonbiju <adonbiju@gmail.com>
2023-02-07 17:07:05 +0530

    In FriendListWidget, Friend Component is added.

--- client/src/scenes/HomePage/index.jsx
+++ client/src/scenes/HomePage/index.jsx
@@ -4,6 +4,7 @@ import UserWidget from 'scenes/widgets/UserWidget'
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 import { useSelector } from "react-redux";
 const HomePage = () => {
@@ -32,7 +33,9 @@ const HomePage = () => {
           <Box flexBasis="26%">
             <AdvertWidget />
             <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
           </Box>
+          
         )}
         
     </Box>
