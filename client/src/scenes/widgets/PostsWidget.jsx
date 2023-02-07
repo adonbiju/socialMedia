7a31c795612d4c7a23ab4bee9e5d4041d529c5f5
adonbiju <adonbiju@gmail.com>
2023-02-07 08:33:57 +0530

    In PostsWidget.jsx, PostWidget is added.

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -22,16 +22,50 @@ const PostsWidget = ({ userId, isProfile = false }) => {
             `http://localhost:3001/posts/${userId}/posts`,
             {
                 method: "GET",
-                 headers: { Authorization: `Bearer ${token}` },
+                headers: { Authorization: `Bearer ${token}` },
             }
         );
         const data = await response.json();
         dispatch(setPosts({ posts: data }));
       };
+    
+      useEffect(() => {
+        if (isProfile) {
+          getUserPosts();
+        } else {
+          getPosts();
+        }
+      }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <>
-         user
+         {posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+        }) => (
+          <PostWidget
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={`${firstName} ${lastName}`}
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}
+            likes={likes}
+            comments={comments}
+          />
+        )
+      )}
         </>
       );
     };
