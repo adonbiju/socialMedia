e609f72de5315dccdc3482b029b5cca7471e371f
adonbiju <adonbiju@gmail.com>
2023-02-07 14:42:41 +0530

    In PostWidget, description and Post Picture has been added.

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -48,12 +48,26 @@ const PostWidget = ({
       dispatch(setPost({ post: updatedPost }));
     };
     return (
-        <div><Friend
+      <WidgetWrapper m="2rem 0">
+        <Friend
         friendId={postUserId}
         name={name}
         subtitle={location}
         userPicturePath={userPicturePath}
-      /></div>
+      />
+      <Typography color={main} sx={{ mt: "1rem" }}>
+        {description}
+      </Typography>
+      {picturePath && (
+        <img
+          width="100%"
+          height="auto"
+          alt="post"
+          style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+          src={`http://localhost:5000/assets/${picturePath}`}
+        />
+      )}
+      </WidgetWrapper>
     )
   }
   export default PostWidget;
