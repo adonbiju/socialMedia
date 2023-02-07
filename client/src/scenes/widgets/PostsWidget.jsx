f3e71a7b2f8aa4c6f5ec1d23d7d84754ed224ee5
adonbiju <adonbiju@gmail.com>
2023-02-07 08:25:49 +0530

    In PostsWidget.jsx , fetch only the specfic user post

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -17,6 +17,18 @@ const PostsWidget = ({ userId, isProfile = false }) => {
     dispatch(setPosts({ posts: data }));
     };
 
+    const getUserPosts = async () => {
+        const response = await fetch(
+            `http://localhost:3001/posts/${userId}/posts`,
+            {
+                method: "GET",
+                 headers: { Authorization: `Bearer ${token}` },
+            }
+        );
+        const data = await response.json();
+        dispatch(setPosts({ posts: data }));
+      };
+
     return (
         <>
          user
