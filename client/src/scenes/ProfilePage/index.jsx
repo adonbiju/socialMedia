ca52680d0ccdb60cf8f6c8c8f45da15faf946b04
adonbiju <adonbiju@gmail.com>
2023-02-07 17:40:14 +0530

    In ProfilePage, 1)Navbar,2)PostsWidget,3)MyPostWidget,4)FriendListWidget,5)UserWidget are added.

--- client/src/scenes/ProfilePage/index.jsx
+++ client/src/scenes/ProfilePage/index.jsx
@@ -15,7 +15,7 @@ const ProfilePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
   const getUser = async () => {
-    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+    const response = await fetch(`http://localhost:5000/user/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
@@ -29,7 +29,30 @@ const ProfilePage = () => {
 
   if (!user) return null;
   return (
-    <div>ProfilePage</div>
+    <Box>
+    <Navbar />
+    <Box
+      width="100%"
+      padding="2rem 6%"
+      display={isNonMobileScreens ? "flex" : "block"}
+      gap="2rem"
+      justifyContent="center"
+    >
+      <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+        <UserWidget userId={userId} picturePath={user.picturePath} />
+        <Box m="2rem 0" />
+        <FriendListWidget userId={userId} />
+      </Box>
+      <Box
+        flexBasis={isNonMobileScreens ? "42%" : undefined}
+        mt={isNonMobileScreens ? undefined : "2rem"}
+      >
+        <MyPostWidget picturePath={user.picturePath} />
+        <Box m="2rem 0" />
+        <PostsWidget userId={userId} isProfile />
+      </Box>
+    </Box>
+  </Box>
   )
 }
 
