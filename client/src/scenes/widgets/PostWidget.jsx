38587f104ec6f6ad4720f20718ea64b329f2c3f9
adonbiju <adonbiju@gmail.com>
2023-02-07 14:34:19 +0530

    In PostWidget, like api has been added.

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -1,4 +1,16 @@
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
 import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
 
 const PostWidget = ({
     postId,
@@ -12,6 +24,29 @@ const PostWidget = ({
     comments,
   }) => 
   {
+    const [isComments, setIsComments] = useState(false);
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.token);
+    const loggedInUserId = useSelector((state) => state.user._id);
+    const isLiked = Boolean(likes[loggedInUserId]);
+    const likeCount = Object.keys(likes).length;
+  
+    const { palette } = useTheme();
+    const main = palette.neutral.main;
+    const primary = palette.primary.main;
+
+    const patchLike = async () => {
+      const response = await fetch(`http://localhost:5000/posts/${postId}/like`, {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId: loggedInUserId }),
+      });
+      const updatedPost = await response.json();
+      dispatch(setPost({ post: updatedPost }));
+    };
     return (
         <div><Friend
         friendId={postUserId}
