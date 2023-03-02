e8f5efe8d4e96a6607c659fc4bb04b63523031f4
adonbiju <adonbiju@gmail.com>
2023-03-02 12:37:22 +0530

    In ProfileCoverPhotoWidget.jsx, Add Or Remove friend functinality added.

--- client/src/widgets/ProfileCoverPhotoWidget.jsx
+++ client/src/widgets/ProfileCoverPhotoWidget.jsx
@@ -1,18 +1,29 @@
 
 import { WidgetWrapper ,Popup,UploadPhoto} from "components";
-import { Box, useMediaQuery, useTheme, Typography, Button, Stack } from "@mui/material";
-import { useSelector } from "react-redux";
+import { Box, useMediaQuery, useTheme, Typography, Button, Stack,Backdrop,CircularProgress } from "@mui/material";
+import { useSelector ,useDispatch} from "react-redux";
 import { useEffect, useState } from "react";
+import { AddOrRemoveFriendApi } from "helper/api";
+import { setFriends } from "state";
+import {useSnackbar} from "notistack" 
 
 const ProfileCoverPhotoWidget = ({userId}) => {
   const { palette } = useTheme();
   const main = palette.neutral.main;
   const isMobileScreens = useMediaQuery("(max-width:1000px)");
-  const [openCoverPhotoPopup,setOpenCoverPhotoPopup]=useState(false)
-  const {_id,coverPhotoPath} = useSelector((state) => state.user);
-  const token = useSelector((state) => state.token);
-  const [user, setUser] = useState(null);
   
+  const [user, setUser] = useState(null);
+  const [openCoverPhotoPopup,setOpenCoverPhotoPopup]=useState(false)
+  const [backDrop,setBackDrop]=useState(false)
+  const {_id,coverPhotoPath} = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+
+  const friends = useSelector((state) => state.user.friends);
+  const isFriend = friends.find((friend) => friend._id === userId);
+  const {enqueueSnackbar} = useSnackbar();
+
+  console.log(isFriend)
   const getUser = async () => {
     const response = await fetch(`http://localhost:5000/user/${userId}`, {
       method: "GET",
@@ -22,6 +33,17 @@ const ProfileCoverPhotoWidget = ({userId}) => {
     setUser(data);
   };
 
+  const patchFriend = async () => {
+    setBackDrop(true);
+    const data = await AddOrRemoveFriendApi(_id,userId,token);
+    dispatch(setFriends({ friends: data }));
+    setBackDrop(false);
+    if (isFriend){
+      enqueueSnackbar(user.firstName+ (" Removed as your Friend Successfully!!"), { variant: 'warning',anchorOrigin:{ vertical: "top", horizontal: "right" } });
+    } else {
+      enqueueSnackbar(user.firstName+ (" Added as your Friend Successfully!!"), { variant: 'success',anchorOrigin:{ vertical: "top", horizontal: "right" } });
+    }
+  };
   useEffect(() => {
     getUser();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
@@ -62,14 +84,23 @@ const ProfileCoverPhotoWidget = ({userId}) => {
             <Typography color={main} variant="h5" fontWeight="500" >
               {user.friends.length} Friends
             </Typography>
-            {isMobileScreens && <Button variant="contained" size="small" sx={{ color: palette.background.alt, backgroundColor: palette.primary.main, borderRadius: "3rem", }} >
-              Add Friend
-            </Button>}
+            {userId===_id?(''):
+            (
+            <>{isMobileScreens && 
+            <Button onClick={() => patchFriend()} variant="contained" size="small" sx={{ color: palette.background.alt, backgroundColor: palette.primary.main, borderRadius: "3rem", }} >
+              {isFriend? "Remove Friend":"Add Friend"}
+            </Button>}</>
+            )}
+
           </Stack>
-          <Stack mt={"4.8rem"} >
-            {!isMobileScreens && <Button variant="contained" size="small" sx={{ color: palette.background.alt, backgroundColor: palette.primary.main, borderRadius: "3rem", }} >
-              Add Friend
-            </Button>}
+        <Stack mt={"4.8rem"} >
+            {userId===_id?(''):
+            (
+            <>{!isMobileScreens && 
+            <Button onClick={() => patchFriend()} variant="contained" size="small" sx={{ color: palette.background.alt, backgroundColor: palette.primary.main, borderRadius: "3rem", }} >
+              {isFriend? "Remove Friend":"Add Friend"}
+            </Button>}</>
+            )}
           </Stack>
         </Box>
 
@@ -77,6 +108,12 @@ const ProfileCoverPhotoWidget = ({userId}) => {
       <Popup title="Upload your cover photo " openPopup={openCoverPhotoPopup}  setOpenPopup={setOpenCoverPhotoPopup}  >
         <UploadPhoto/>
       </Popup>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={backDrop}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </WidgetWrapper>
   );
 }
