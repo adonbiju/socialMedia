7c68088d9fae7cc1af6d0053ed4e81c0aa95ce59
adonbiju <adonbiju@gmail.com>
2023-03-04 10:55:30 +0530

    In backend, addRemoveFriend api bug has been solved

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -49,7 +49,7 @@ export const addRemoveFriend = async (req, res) => {
 
     if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((id) => id !== friendId);
-      friend.friends = friend.friends.filter((id) => id !== id);
+      friend.friends = friend.friends.filter((fid) => fid !== id);
     } else {
       user.friends.push(friendId);
       friend.friends.push(id);
